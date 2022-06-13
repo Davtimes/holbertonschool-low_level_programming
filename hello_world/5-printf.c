0c873558a1b14687e9375220735cc5860209ee1c
Davtimes <davidroar.2002@gmail.com>
2022-06-13 15:52:07 -0500

    Holberton scholl-low_level_programming

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-Imprime la frase :with proper grammar, but the outcome is a piece of art,
+ * Return: Se deja en 0 puesto que la funcion esta vacia
+ */
+ int main (void)
+ {
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return(0);
+
+ }
+ 
