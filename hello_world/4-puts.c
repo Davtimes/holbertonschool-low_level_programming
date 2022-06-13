d176918e5d12aaa0f76fc8a0d28b631ebcedc6ce
Davtimes <davidroar.2002@gmail.com>
2022-06-13 15:45:12 -0500

    Holberton scholl-low_level_programming

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+
+/**
+ * Imprime la frase :"Programming is like building a multilingual puzzle
+ * Return: Se deja en 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
