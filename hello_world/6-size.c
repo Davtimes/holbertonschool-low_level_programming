15a48aa8a45888b17414d58f7d399b362c120a08
Davtimes <davidroar.2002@gmail.com>
2022-06-13 16:12:50 -0500

    Holberton scholl-low_level_programming

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-Imprime el tamaño en bytes de algunos tipos de datos
+ * Return: Se deja en 0 puesto que la funcion esta vacia
+ */
+ int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
