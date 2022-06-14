dc7d6be7d0ceda148e0e813304cbe684f3fe637d
Davtimes <davidroar.2002@gmail.com>
2022-06-14 16:02:02 -0500

    C - Variables, if, else, while

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Escribe todo el alfabeto
+ * main - Escribe todo el alfabeto sin las letras e y q
  * Return: retorna cero 0
 */
 int main(void)
@@ -8,6 +8,7 @@ int main(void)
 	char az;
 
 	for (az = 'a'; az <= 'z'; az++)
+	if (az != 'e' && az != 'q')
 	putchar(az);
 	putchar('\n');
 	return (0);
