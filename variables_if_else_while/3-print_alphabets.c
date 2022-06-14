bd9190a482edcf2ff6a26e92ea9121c50ff46e7d
Davtimes <davidroar.2002@gmail.com>
2022-06-14 15:55:11 -0500

    C - Variables, if, else, while

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Escribe todo el alfabeto con algunas letras en mayuscula
+ * Return: retorna cero 0
+*/
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	putchar(az);
+	for (az = 'A'; az <= 'Z'; az++)
+	putchar(az);
+	putchar('\n');
+	return (0);
+}
