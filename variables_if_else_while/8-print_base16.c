d2c09415cf459e6c838003d66aec44c22f493f46
Davtimes <davidroar.2002@gmail.com>
2022-06-14 17:34:53 -0500

    C - Variables, if, else, while

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Escribe numeros de 0 a 9
+ * Return: retorna cero 0
+*/
+int main(void)
+{
+	int az;
+	char l;
+
+for (az = 0; az <= 9; az++)
+	putchar(az + '0');
+for (l = "a"; l <= "f"; l++)
+	putchar(l);
+	putchar('\n');
+	return (0);
+}
