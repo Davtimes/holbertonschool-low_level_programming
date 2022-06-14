39f5432f47b29d93c763f3e7e767021c2da8f5d9
Davtimes <davidroar.2002@gmail.com>
2022-06-14 16:44:31 -0500

    C - Variables, if, else, while

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Escribe todo el alfabeto sin las letras e y q
+ * main - Escribe numeros de 0 a 9
  * Return: retorna cero 0
 */
 int main(void)
 {
 	int az;
 
-	for (az = 0; az <= 9; az++)
+for (az = 0; az <= 9; az++)
 printf("%d", az);
 	putchar('\n');
 	return (0);
