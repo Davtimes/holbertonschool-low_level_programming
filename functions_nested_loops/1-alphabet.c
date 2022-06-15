ce0b0c83ba22504bdb9d04629934cdbcf73a4360
Davtimes <davidroar.2002@gmail.com>
2022-06-15 14:00:36 -0500

    C - Functions, nested loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main- Imprime _putchar seguido por una nueva linea
+ *
+ * Return: retorna 0
+ */
+void print_alphabet(void)
+{
+
+	char a;
+
+	for (a = 'a'; n <= 'z'; n++)
+		_putchar(n);
+
+	_putchar('\n');
+}
