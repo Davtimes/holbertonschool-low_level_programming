2497f2b779664a22695e1288aeb3ed9460aa3084
Davtimes <davidroar.2002@gmail.com>
2022-06-15 13:58:29 -0500

    C - Functions, nested loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * main - Imprime "_putchar" con una nuevalinea al final
- * Return: Retorna 
+ * main- Imprime _putchar seguido por una nueva linea
+ *
+ * Return: retorna 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	_putchar('\n');
-
-	return (0);
-}    
+return (0);
+}
