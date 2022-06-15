d7aa262a547d96338c82349cf01ce9df5ccd0235
Davtimes <davidroar.2002@gmail.com>
2022-06-15 14:31:34 -0500

    C - Functions, nested loops

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  * _islower - Imprime el alfabeto
+  * _isalpha - Imprime el alfabeto
   * @c: valor condicional
   * Return: 0 o 1
   */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 'a' && c < 'z') || (c >= 'A' && c < 'Z')
 		return (1);
