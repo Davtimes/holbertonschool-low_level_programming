51d4da8f13fb4f3576810ae23448e185a6d1bbe4
Davtimes <davidroar.2002@gmail.com>
2022-06-15 15:58:55 -0500

    C - Functions, nested loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Selecciona dos enteros y retorna el resultdado de la suma de ambos
+ *@x: Primer valor adicionado
+ *@y: Sgundo valor adicionado
+ * Return: 0
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
