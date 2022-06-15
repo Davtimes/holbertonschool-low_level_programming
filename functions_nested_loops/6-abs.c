4c36dfec40f4e4dca1ef1bf4039ec5b3c94edc12
Davtimes <davidroar.2002@gmail.com>
2022-06-15 14:44:49 -0500

    C - Functions, nested loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+  *  _abs - cumputa el valor absoluto de un entero int (integer)
+  * @p: valor a condicionar
+  * Return: 0 o 1
+  */
+int _abs(int p)
+{
+	if (p < 0)
+	{
+		p = p * -1;
+	}
+	return (p);
+}
