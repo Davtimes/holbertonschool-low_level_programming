28434b93e761bcca38a662ecbd4d202fc4d0aa4b
Davtimes <davidroar.2002@gmail.com>
2022-06-15 13:32:19 -0500

    C - Variables, if, else, while

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Imprime "_putchar" con una nuevalinea al final
+ * Return: Retorna 
+ */
+int main(void)
+{
+    printf("_putchar");
+    putchar('\n');
+    return (0);
+
+}
